c7b521285fac04ae68f32e4dc14bbb156e478ed7
Mohammad Parvez <parvez0011@gmail.com>
2020-01-31 16:13:22 +0530

    updated script

--- src/test/java/TFSAutomation/tfs/Session.java
+++ src/test/java/TFSAutomation/tfs/Session.java
@@ -3,6 +3,10 @@ package TFSAutomation.tfs;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -12,7 +16,9 @@ import java.util.Properties;
 public class Session {
 	
 	static Properties prop = new Properties();
+	static Properties log = new Properties();
 	static String defaultConfigFile="./Config.properties";
+	static String logFile="./logFile.properties";
 	static public Map<String, String> _getSessionConfig() {
         String[] configKeys = {"Release", "Sprint", "AppUrl", "UserName", "Password","Testplan","TestType"};
         Map<String, String> config;
@@ -35,4 +41,65 @@ public class Session {
             return null;
         }
     }
+	
+	/*
+    Create new property file if does not exist
+    */
+   
+	public static void createNewPropertyFile() {
+       File propertyFile = new File(logFile);
+       try {
+           if(!propertyFile.exists()) 
+           {
+               System.out.println("####################################################################");
+               System.out.println("Creating new log file...");
+               propertyFile.createNewFile();
+           } 
+           else
+           {
+               System.out.println("log file is already created");
+           }
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+   }
+	
+	 /**
+     * This method is used to store a new property in the properties file.
+     *
+     * @param property : name for the new property.
+     * @param value : value for the new property.
+     * @throws IOException
+     */
+    public static void writeProperty(String property, String value) {
+        // Write properties file.
+        // OutputStream outPropFile = null;
+    	clearProperty();
+
+        try {
+            InputStream inPropFile = new FileInputStream(logFile);
+            log.load(inPropFile);
+            inPropFile.close();
+            OutputStream outPropFile = new FileOutputStream(logFile);
+            // System.getProperty("user.dir")+"\\AnswerKeys\\"+top+".properties"
+
+            log.setProperty(property, value);
+            log.store(outPropFile, null);
+            outPropFile.close();
+        } catch (IOException e) {
+        }
+    }
+    
+    public static void clearProperty() {
+        try {
+            InputStream inPropFile = new FileInputStream(logFile);
+            log.load(inPropFile);
+            inPropFile.close();
+            OutputStream outPropFile = new FileOutputStream(logFile);
+            log.clear();
+            outPropFile.close();
+        } catch (IOException e) {
+        }
+    }
+
 }
--- src/test/java/TFSAutomation/tfs/UpdateAnalysis.java
+++ src/test/java/TFSAutomation/tfs/UpdateAnalysis.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Set;
 
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -24,34 +23,34 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class UpdateAnalysis {
-	
+
 	static WebDriver driver;
 
 	@Test
 	public void main1() throws InterruptedException, FileNotFoundException, IOException {
-		
-		Map<String, String> config=Session._getSessionConfig();
-		String Release=config.get("Release");
-		String Sprint=config.get("Sprint");
-		String AppUrl=config.get("AppUrl");
-		String uname=config.get("UserName");
-		String pword=config.get("Password");
-		String tPlan=config.get("Testplan");
-		String TestType=config.get("TestType");
-				
+
+		Map<String, String> config = Session._getSessionConfig();
+		String Release = config.get("Release");
+		String Sprint = config.get("Sprint");
+		String AppUrl = config.get("AppUrl");
+		String uname = config.get("UserName");
+		String pword = config.get("Password");
+		String tPlan = config.get("Testplan");
+		String TestType = config.get("TestType");
+
 //		String Release="Release 2.1";
 //		String Sprint="Sprint 29";
-		
-		System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\selenium-drivers\\chromedriver.exe"); 
-		
+
+		System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\selenium-drivers\\chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.manage().window().maximize();	
+		driver.manage().window().maximize();
 		driver.get(AppUrl);
+		driver.findElement(By.xpath("//input[@type='email']")).clear();
 		driver.findElement(By.xpath("//input[@type='email']")).sendKeys(uname.trim());
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//input[@type='submit']")).click();
 		Thread.sleep(10000);
-		
+
 		driver.findElement(By.id("passwordInput")).sendKeys(pword.trim());
 		Thread.sleep(5000);
 		driver.findElement(By.xpath("//span[@id='submitButton']")).click();
@@ -59,113 +58,116 @@ public class UpdateAnalysis {
 //		
 		driver.findElement(By.xpath(".//span[contains(text(),'Test Plans')]")).click();
 		Thread.sleep(5000);
-		
-		WebElement TestPlanAnchor=driver.findElement(By.xpath(".//div[contains(text(),'"+tPlan+"')]/parent::a"));
-		
-		System.out.println("##### 1"+TestPlanAnchor.getAttribute("aria-expanded"));
-		System.out.println("##### 2"+TestPlanAnchor.getAttribute("aria-expanded").toString());
-		System.out.println("##### 3"+TestPlanAnchor.getAttribute("aria-expanded").equals("false"));
-		
-		
-		if(TestPlanAnchor.getAttribute("aria-expanded").equals("false"))
-		driver.findElement(By.xpath(".//div[contains(text(),'"+tPlan+"')]/span")).click();
 
-		
-WebElement TestingTypeAnchor=driver.findElement(By.xpath(".//div[contains(text(),'"+TestType+"')]/parent::a"));
-				
-		if(TestingTypeAnchor.getAttribute("aria-expanded").equals("false"))
-		driver.findElement(By.xpath(".//div[contains(text(),'"+TestType+"')]/span")).click();
-		
-		
-		WebElement ReleaseAnchor=driver.findElement(By.xpath(".//div[contains(text(),'"+Release+"')]/parent::a"));
-		
-		if(ReleaseAnchor.getAttribute("aria-expanded").equals("false"))
-		driver.findElement(By.xpath(".//div[contains(text(),'"+Release+"')]/span")).click();
-		
-		
+		WebElement TestPlanAnchor = driver.findElement(By.xpath(".//div[contains(text(),'" + tPlan + "')]/parent::a"));
+
+		System.out.println("##### 1" + TestPlanAnchor.getAttribute("aria-expanded"));
+		System.out.println("##### 2" + TestPlanAnchor.getAttribute("aria-expanded").toString());
+		System.out.println("##### 3" + TestPlanAnchor.getAttribute("aria-expanded").equals("false"));
+
+		if (TestPlanAnchor.getAttribute("aria-expanded").equals("false"))
+			driver.findElement(By.xpath(".//div[contains(text(),'" + tPlan + "')]/span")).click();
+
+		WebElement TestingTypeAnchor = driver
+				.findElement(By.xpath(".//div[contains(text(),'" + TestType + "')]/parent::a"));
+
+		if (TestingTypeAnchor.getAttribute("aria-expanded").equals("false"))
+			driver.findElement(By.xpath(".//div[contains(text(),'" + TestType + "')]/span")).click();
+
+		WebElement ReleaseAnchor = driver.findElement(By.xpath(".//div[contains(text(),'" + Release + "')]/parent::a"));
+
+		if (ReleaseAnchor.getAttribute("aria-expanded").equals("false"))
+			driver.findElement(By.xpath(".//div[contains(text(),'" + Release + "')]/span")).click();
+
 //		List<WebElement> sprint= driver.findElements(By.xpath(".//div[contains(text(),'Release 2.1')]/parent::a/following-sibling::ul/li"));
-		List<WebElement> sprints= driver.findElements(By.xpath(".//div[contains(text(),'"+Release+"')]/parent::a/following-sibling::ul/li"));
+		List<WebElement> sprints = driver.findElements(
+				By.xpath(".//div[contains(text(),'" + Release + "')]/parent::a/following-sibling::ul/li"));
 //		
-		for(WebElement sprintEle : sprints)
-		{String sprintId=sprintEle.getAttribute("id");
-			String loc=".//li[@id='"+sprintId+"']/a//span";
-			
-			List<WebElement> expand = driver.findElements(By.xpath(".//li[@id='"+sprintId+"']/ul"));
-			
-			if (expand.size()==1)
-				
-			Thread.sleep(1000);
-			if(Sprint.trim().equalsIgnoreCase(driver.findElement(By.xpath(".//li[@id='"+sprintId+"']/a/div")).getText().trim()))
-			{
-				
-				if (expand.size()!=1)
-			driver.findElement(By.xpath(loc)).click();
-			Thread.sleep(5000);
-			List<WebElement> story = driver.findElements(By.xpath(".//li[@id='"+sprintId+"']/ul/li"));
-			
-			for(WebElement storyEle :  story)
-			{
-				storyEle.click();
-				Thread.sleep(3000);
-				List<WebElement> testcase=driver.findElements(By.className("grid-cell-contents-container"));
-			
-				for(WebElement testcaseEle : testcase)
-				{
-					Actions rClick=new Actions(driver); 
-					rClick.contextClick(testcaseEle).build().perform();
-					
-					Thread.sleep(1000);
-					driver.findElement(By.xpath(".//span[contains(text(),'View test result')]")).click();
-					Thread.sleep(2000);
-					driver.switchTo().window(driver.getWindowHandles().toArray()[1].toString());
-					Thread.sleep(2000);
-					List<WebElement> attachment = driver.findElements(By.xpath(".//td[@class='iteration-attachment-name']/a"));
-					List<WebElement> comment = driver.findElements(By.xpath(".//p[contains(text(),'Post Review Done')]"));
-					
-					System.out.println("#########attachment - "+attachment.size());
-					System.out.println("#########comment - "+comment.size());
-					if(attachment.size()!=0 && comment.size()==0)
-					{
-					driver.findElement(By.xpath(".//span[contains(text(),'Update analysis')]")).click();
+		for (WebElement sprintEle : sprints) {
+			String sprintId = sprintEle.getAttribute("id");
+			String loc = ".//li[@id='" + sprintId + "']/a//span";
 
-					Thread.sleep(5000);
-					WebElement ele1=driver.findElement(By.xpath(".//*[@class='identity-picker-dropdown identity-picker-fixed-position']"));
-			
-					driver.findElement(By.xpath(".//div[@class='identity-picker-container']/div/span[2]")).click();
-					
-					
-					
-					WebElement ele2=driver.findElement(By.xpath(".//div[@class='item-text-container']//span[contains(text(),'Mohammad Parvez')]/parent::div/parent::div/preceding-sibling::div[2]"));
-			
-					
-					Thread.sleep(1000);
-					WebElement element = driver.findElement(By.xpath(".//div[@class='item-text-container']//span[contains(text(),'Mohammad Parvez')]"));
-					rClick.moveToElement(element).build().perform();
-					
-					driver.findElement(By.xpath(".//div[@class='item-text-container']//span[contains(text(),'Mohammad Parvez')]")).click();
-					driver.findElement(By.id("comment")).sendKeys("Post Review Done.");
-					driver.findElement(By.id("ok")).click();
-					Thread.sleep(1000);
-					driver.close();
-					Thread.sleep(1000);
-					driver.switchTo().window(driver.getWindowHandles().toArray()[0].toString());
-					}
-					else
-					{
-						driver.close();
+			List<WebElement> expand = driver.findElements(By.xpath(".//li[@id='" + sprintId + "']/ul"));
+
+			if (expand.size() == 1)
+
+				Thread.sleep(1000);
+			if (Sprint.trim().equalsIgnoreCase(
+					driver.findElement(By.xpath(".//li[@id='" + sprintId + "']/a/div")).getText().trim())) {
+
+				if (expand.size() != 1)
+					driver.findElement(By.xpath(loc)).click();
+				Thread.sleep(5000);
+				List<WebElement> story = driver.findElements(By.xpath(".//li[@id='" + sprintId + "']/ul/li"));
+
+				for (WebElement storyEle : story) {
+					storyEle.click();
+					Thread.sleep(3000);
+					List<WebElement> testcase = driver.findElements(By.xpath(".//span[contains(text(),'Passed')]"));
+
+					for (WebElement testcaseEle : testcase) {
+						Actions rClick = new Actions(driver);
+						rClick.contextClick(testcaseEle).build().perform();
+
 						Thread.sleep(1000);
-						driver.switchTo().window(driver.getWindowHandles().toArray()[0].toString());
+						driver.findElement(By.xpath(".//span[contains(text(),'View test result')]")).click();
+						Thread.sleep(2000);
+						driver.switchTo().window(driver.getWindowHandles().toArray()[1].toString());
+						Thread.sleep(2000);
+						List<WebElement> attachment = driver
+								.findElements(By.xpath(".//td[@class='iteration-attachment-name']/a"));
+						List<WebElement> comment = driver
+								.findElements(By.xpath(".//p[contains(text(),'Post Review Done')]"));
+
+						System.out.println("#########attachment - " + attachment.size());
+						System.out.println("#########comment - " + comment.size());
+						if (attachment.size() != 0 && comment.size() == 0) {
+
+							driver.findElement(By.xpath(".//span[contains(text(),'Update analysis')]")).click();
+
+							Thread.sleep(5000);
+							WebElement ele1 = driver.findElement(
+									By.xpath(".//*[@class='identity-picker-dropdown identity-picker-fixed-position']"));
+
+							driver.findElement(By.xpath(".//div[@class='identity-picker-container']/div/span[2]"))
+									.click();
+
+							WebElement ele2 = driver.findElement(By.xpath(
+									".//div[@class='item-text-container']//span[contains(text(),'Mohammad Parvez')]/parent::div/parent::div/preceding-sibling::div[2]"));
+
+							Thread.sleep(1000);
+							WebElement element = driver.findElement(By.xpath(
+									".//div[@class='item-text-container']//span[contains(text(),'Mohammad Parvez')]"));
+							rClick.moveToElement(element).build().perform();
+
+							driver.findElement(By.xpath(
+									".//div[@class='item-text-container']//span[contains(text(),'Mohammad Parvez')]"))
+									.click();
+							driver.findElement(By.id("comment")).sendKeys("Post Review Done.");
+							driver.findElement(By.id("ok")).click();
+							Thread.sleep(1000);
+							driver.close();
+							Thread.sleep(1000);
+							driver.switchTo().window(driver.getWindowHandles().toArray()[0].toString());
+						} else {
+							String noAttachment = driver.findElement(By.xpath(".//td[@class='query-title']//a")).getText();
+							Session.writeProperty("Backlog ID: ", noAttachment);
+							driver.close();
+							Thread.sleep(1000);
+							driver.switchTo().window(driver.getWindowHandles().toArray()[0].toString());
+						}
 					}
-					}
-			
+
+				}
+
 			}
-			
-		}}
-		
+		}
+
 	}
+
 	static void setAttributeByJavaScript(WebElement element, String attName, String attValue) {
-		((JavascriptExecutor) driver).executeScript("arguments[0].setAttribute(arguments[1], arguments[2]);", 
-                element, attName, attValue);
-   }
+		((JavascriptExecutor) driver).executeScript("arguments[0].setAttribute(arguments[1], arguments[2]);", element,
+				attName, attValue);
+	}
 
 }
